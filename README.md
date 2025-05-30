8b508433a3a158c1b22747e119ba846267942c9d
amnesiacsloth <amnesiacsloth@gmail.com>
2025-05-29 23:35:47 -0700

    Fix: Prep filestructure for push to dev

--- README.md
+++ README.md
@@ -1,58 +1,12 @@
-# Chrisvelasquez.dev
+# React + Vite
 
-## Key Features
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
-Written in React :)
+Currently, two official plugins are available:
 
-## Credits 
-This site uses the following tools and technologies
-* Vite
-* React Js
-* Tailwind CSS
-* Npm 
-* Major League Baseball API 
-* DateTime API
-* Github Actions
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
 
+## Expanding the ESLint configuration
 
-### General Structure of my page
-
-* High level body that has a cool gradient? 
-* Grid that holds all my components with different sizing?
-  * Welcome Box
-    * Contact Boxes
-      * github
-      * linkedin
-      * discord
-      * etc
-  * Contact with socials, thin
-  * Local weather / Time in timezone / converted to UTC
-  * Project 1 - BLE sensor 
-  * Project 2 - cogs108
-  * Project 3 - idk website
-  * Theme changer
-  * About me Block 
-    * Quick overview, like name, dev type, stack, education, country
-    * tools / technologies
-    * Beyond dev work interests
-  * Resume box
-  * Small credits box  
-
-## Working with MLB public API endpoints
-
-### Useful codes for my lil Dodgers widget
-
-Dodgers team ID: 119
-
-MLB league ID: sportsId=1
-
-Get previous 5 games from game schedule
-
-`https://statsapi.mlb.com/api/v1/teams/<teamId>?hydrate=previousSchedule`
-
-# TODO 
-- NOTE: When will mlb api update
-  - around 10am pst 
-
-
-
+If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
---
+++ src/components/Data.jsx
@@ -0,0 +1,88 @@
+const personalLinks = {
+  github: "https://github.com/AmnesiacSloth",
+  linkedin: "https://www.linkedin.com/in/chrisvelasquez-24/",
+  youtube: "https://www.youtube.com/@aslothcodes",
+  resume: Resume,
+}
+
+//
+// ----- ABOUT ME BUTTON DATA ----- //
+//
+
+/* Svg to react component imports via react-svgr?*/
+import LinkedinIcon from "../assets/svgs/linkedin-icon.svg?react"
+import GithubIcon from "../assets/svgs/github-icon.svg?react"
+import ResumeIcon from "../assets/svgs/resume-icon.svg?react"
+import Resume from "../assets/ChristianVelasquezResume-5-3-25.pdf"
+
+export const buttonIcons = [{
+  id: 0,
+  component: LinkedinIcon,
+  resourceLink: personalLinks.linkedin,
+}, {
+  id: 1,
+  component: GithubIcon,
+  resourceLink: personalLinks.github,
+}, {
+  id: 2,
+  component: ResumeIcon,
+  resourceLink: Resume,
+}]
+
+// 
+// ----- PROJECT DATA ----- //
+//
+
+/* Image imports */
+import mcuImg from "../assets/stm32.jpg"
+import dsGasImg from "../assets/gasdatascience.jpg"
+import portfolioSiteImg from "../assets/portfoliosite.jpeg"
+
+const projectlinks = {
+  smt32blesensor: "https://github.com/cse-190e-fall-2023/youlostit-project-1-blinkenlights-32-bit-monstas",
+  portfoliowebsite: "https://github.com/AmnesiacSloth/ChrisVelasquez.dev",
+  httpserver: "https://github.com/AmnesiacSloth/httpserver",
+  jccprocessor: "https://github.com/AmnesiacSloth/CPU-Design",
+  bingobongo: "https://github.com/AmnesiacSloth/BingoBongo",
+  dimes: "https://github.com/AmnesiacSloth/Dimes",
+}
+
+export const projectData = [{
+  id: 0,
+  image: mcuImg,
+  githublink: projectlinks.smt32blesensor,
+  mobileTitle: "BLE-Enabled Device Locator",
+  mobileDesc: "STM32 Microcontroller, Register-Level Firmware in C, tested using a Digital Logic Analyzer",
+}, {
+  id: 1,
+  image: dsGasImg,
+  githublink: "https://github.com/COGS108/Group_Sp23_COGS108_Project_Group",
+  mobileTitle: "Russia-Ukraine Crisis Analysis",
+  mobileDesc: "EDA using Python3, Jupyter Notebooks, Pandas, Scipy, and Seaborn ",
+
+}, {
+  id: 2,
+  image: portfolioSiteImg,
+  githublink: projectlinks.portfoliowebsite,
+  mobileTitle: "Portfolio Website",
+  mobileDesc: "Portfolio site built using Vite, React, Javascript, TailwindCSS and deployed on Github Pages",
+
+}]
+
+
+//
+// ----- CONTACT DATA -----
+//
+export const contactData = [{
+  id: 0,
+  site: "LinkedIn",
+  link: personalLinks.linkedin,
+}, {
+  id: 1,
+  site: "Github",
+  link: personalLinks.github,
+}, {
+  id: 2,
+  site: "Youtube",
+  link: personalLinks.youtube,
+}]
